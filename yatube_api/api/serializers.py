d48e689ef774475f45370d491c0ba5d15058c0b4
Mikhail <mishatar2004@gmail.com>
2023-01-23 15:18:36 +0300

    project_sprin9

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 from rest_framework.validators import UniqueTogetherValidator
